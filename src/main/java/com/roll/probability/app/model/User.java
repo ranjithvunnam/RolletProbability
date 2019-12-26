e5640d81d3f6c70818f21be320941c7e77cb590c
Ranjith <ranjithvunnam@gmail.com>
2019-12-26 16:13:27 +0530

    Initial commit.

---
+++ src/main/java/com/roll/probability/app/model/User.java
@@ -0,0 +1,13 @@
+package com.roll.probability.app.model;
+
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
